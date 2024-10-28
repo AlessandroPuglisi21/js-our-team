1784272b253b10b5b78cc415101eee0adae66955
AlessandroPuglisi <alessandropuglisi2000@gmail.com>
2024-10-28 14:30:03 +0100

    ADD: CARDS INFO

--- assets/script.js
+++ assets/script.js
@@ -3,36 +3,62 @@ const teamMembers = [
     name: "Marco Bianchi",
     role: "Designer",
     email: "marcobianchi@team.com",
-    img: "img/male1.png"
+    img: "img/male1.png",
   },
   {
     name: "Laura Rossi",
     role: "Front-end Developer",
     email: "laurarossi@team.com",
-    img: "img/female1.png"
+    img: "img/female1.png",
   },
   {
     name: "Giorgio Verdi",
     role: "Back-end Developer",
     email: "giorgioverdi@team.com",
-    img: "img/male2.png"
+    img: "img/male2.png",
   },
   {
     name: "Marta Ipsum",
     role: "SEO Specialist",
     email: "martarossi@team.com",
-    img: "img/female2.png"
+    img: "img/female2.png",
   },
   {
     name: "Roberto Lorem",
     role: "SEO Specialist",
     email: "robertolorem@team.com",
-    img: "img/male3.png"
+    img: "img/male3.png",
   },
   {
     name: "Daniela Amet",
     role: "Analyst",
     email: "danielaamet@team.com",
-    img: "img/female3.png"
-  }
+    img: "img/female3.png",
+  },
 ];
+
+const teamCardsContainer = document.getElementById("team-cards");
+
+teamMembers.forEach((member) => {
+  const card = document.createElement("div");
+  card.className = "col-md-4 mb-4";
+
+  card.innerHTML = `
+<div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="${member.img}" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">${member.name}</h5>
+        <p class="card-text">${member.role}</p>
+        <p class="card-text"><small class="text-body-secondary">${member.email}</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+  `;
+
+  teamCardsContainer.appendChild(card);
+});
